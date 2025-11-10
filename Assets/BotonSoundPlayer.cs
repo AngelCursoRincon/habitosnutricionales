e1ce689a86c15ddb4f61c69c819e522fadfacca4
AngelCursoRincon <angelcabreragil@alumno.ieselrincon.es>
2025-11-10 17:42:25 +0000

    Añade la manzana y sonidos

--- Assets/BotonSoundPlayer.cs
+++ Assets/BotonSoundPlayer.cs
@@ -7,6 +7,33 @@ public class BotonSoundPlayer : MonoBehaviour
 
     private void OnMouseDown()
     {
+        
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        OnTouchDown();
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTouchDown()
+    {
         source.PlayOneShot(clip);
     }
 }
